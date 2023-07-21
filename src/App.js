233ce3e008b6b671a735b0d4f59d5eb1b7decb63
bastianborndoerfer <bastian.borndoerfer@gmx.de>
2023-07-21 15:39:17 +0200

    finish tasks

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import List from "./components/List/index.js";
 
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
 
+  function handleDeleteTag(tagToDelete) {
+    const updatedTags = tags.filter((tag) => {
+      return tag !== tagToDelete;
+    });
+    setTags(updatedTags);
+  }
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
